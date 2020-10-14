5f966f3b0ccffeca8e7d4c80e87eebba86bedbb5
Ashlee Jollymore <Ashlee.Jollymore@gov.bc.ca>
2020-10-14 10:55:55 -0700

    Small changes

--- R/Render_regionalstreamflow.R
+++ R/Render_regionalstreamflow.R
@@ -13,14 +13,12 @@ start.time1 <- Sys.time()
 devtools::install_github('rstudio/rmarkdown')
 
 # Run the Rmarkdown for the daily statistics files
-rmarkdown::render(paste0("C:/Users/AJOLLYMO/RProjects/hydrolook/inst/templates/regional_streamflow.Rmd"),
-                  output_file = paste0("C:/Users/AJOLLYMO/RProjects/hydrolook/inst/templates/WestCoastStats_",
-                                       Sys.Date(), ".html"),
-                  params = list(region = c("West Coast Natural Resource Region")))
+#rmarkdown::render(paste0("C:/Users/AJOLLYMO/RProjects/hydrolook/inst/templates/regional_streamflow.Rmd"),
+#                  output_file = paste0("C:/Users/AJOLLYMO/RProjects/hydrolook/inst/templates/WestCoastStats_",
+#                                       Sys.Date(), ".html"),
+#                  params = list(region = c("West Coast Natural Resource Region")))
 
 natural_resource_regions = unique(nr_regions()$ORG_UNIT_NAME)
-
-
 
 for(i in 1:length(natural_resource_regions)){
   rmarkdown::render(paste0("C:/Users/AJOLLYMO/RProjects/hydrolook/inst/templates/regional_streamflow.Rmd"),
